a3dffcd71d6496d11571a85a8ab2570738c62ce1
Namita Khanchustambham <nkhanchu@berkeley.edu>
2023-03-08 02:50:33 -0800

    Working on project

---
+++ Static React/counter-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Button';
+import { useState } from 'react';
+import Button from './components/Button';
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount(count + 1);
+  }
+  
+  function Decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+  <div className="App">
+      <p>Count: {count}</p>
+      <Button title={"+"} task={Increment}> </Button>
+      <Button title={"-"} task={Decrement}> </Button>
+  </div>
+  );
+}
+
+export default App;
